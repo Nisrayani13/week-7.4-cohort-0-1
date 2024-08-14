bab4d068ec60e1aa41eebca26878024e3d87c761
srisailam nisrayani <s.nisrayaniraj@gmail.com>
2024-08-14 16:41:18 +0530

    worked with atomFamily and selectorFamily

--- atom-family/src/atoms.ts
+++ atom-family/src/atoms.ts
@@ -1,9 +1,31 @@
-import { atomFamily } from "recoil";
+import { atomFamily, selectorFamily } from "recoil";
 import { TODOS } from "./todos";
+import axios from "axios";
+
+// <---------         ATOM FAMILY   -------------->
+
+// export const todosAtomFamily = atomFamily({
+//   key: 'todosAtomFamily',
+//   default: id => {
+//     return TODOS.find(x => x.id === id)
+//   },
+// });
+
+// <------------- ATOM FAMILY AND SELECTOR FAMILY  ------------------->
 
 export const todosAtomFamily = atomFamily({
-  key: 'todosAtomFamily',
-  default: id => {
-    return TODOS.find(x => x.id === id)
-  },
+  key: "todosAtomFamily",
+  default: selectorFamily({
+    key: "todosAtomFamilySelectorFamily",
+    get: function (id) {
+      return async function ({ get }) {
+        const res = await axios.get(
+          `https://sum-server.100xdevs.com/todo?id=${id}`
+        );
+        return res.data.todo;
+      };
+    },
+  }),
 });
+
+
